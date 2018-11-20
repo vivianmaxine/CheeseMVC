f7413f364dd21d819bd8efb087f5c714cd430774
Vivian Maxine <design@vivianmaxine.com>
2018-11-19 20:23:26 -0600

    Added working validation for empty name field in Add Cheese form

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -11,7 +11,7 @@ namespace CheeseMVC.Controllers
     public class CheeseController : Controller
     {
         // static means it is available to any code within the class
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        private static Dictionary<string, string> Cheeses = new Dictionary<string, string>();
 
         // GET: /<controller>/
         public IActionResult Index()
@@ -24,7 +24,7 @@ namespace CheeseMVC.Controllers
 
             // ViewBag.cheeses from view, Cheeses from controller
             ViewBag.cheeses = Cheeses; // Able to relate list/dictionary in controller to viewer
-
+            ViewBag.namerError = Cheeses; // MAYBE DELETE
             return View();
         }
 
@@ -36,16 +36,64 @@ namespace CheeseMVC.Controllers
 
         // Handles add cheese form submissions and let user add new cheese to list
 
-        [HttpPost]
-        [Route("/Cheese/AddCheese")]
-        public IActionResult NewCheese(string cheesename, string cheesedescription)
-        {
-            // Add new cheese to exisiting cheese list
 
-            Cheeses.Add(cheesename, cheesedescription);
+        // Add a new controller to display the cheese removal form
+        public IActionResult RemoveCheese()
+        {
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        /*
+         Could also do this (versus the above):
+
+         public IActionREsult Index(Dictionary<string, string> cheeses) {
+         
+            ViewBag.Cheeses() = cheeses
+
+            return View();
+
+        }
+         */
+
+
+
+        [HttpPost]
+        // [Route("/Cheese/AddCheese")] << Don't need this
+        public IActionResult AddCheese(string cheesename, string cheesedescription, string nameError)
+        {
+            // Set error to an empty string by default
+            nameError = "";
+
+            // If nothing is listed for the cheese name...
+            if (String.IsNullOrEmpty(cheesename))
+            {
+                ViewBag.nameError = "Please enter a valid name.";
+
+                return View(); // Use this instead of redirecting, which will wipe out the current view
+            }
+
+
+            // Add new cheese to exisiting cheese list
+            else
+            {
+                Cheeses.Add(cheesename, cheesedescription);
+            }
+            return Redirect("/Cheese");
+        }
+
+        [HttpPost]
+        [Route("/Cheese/RemoveCheese")]
+        public IActionResult OldCheese(List<string> cheeseToDelete)
+        {
+            // Remove cheese from cheese list
+            foreach (string cheeseitem in cheeseToDelete)
+            {
+                Cheeses.Remove(cheeseitem);
+            }
 
             return Redirect("/Cheese");
         }
     }
-
 }
